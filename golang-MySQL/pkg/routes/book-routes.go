9cde59696596d00cd2468cb021008fd133c10ada
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-21 22:53:41 +0530

    feat: update book routes to include CRUD operations for books

--- golang-MySQL/pkg/routes/book-routes.go
+++ golang-MySQL/pkg/routes/book-routes.go
@@ -1,7 +1,14 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/Go_Projects/golang-MySQL/pkg/controllers"
+	"github.com/gorilla/mux"
+)
 
 var RegisterBookRoutes = func(router *mux.Router) {
-
+	router.HandleFunc("/getBooks", controllers.GetBook).Methods("Get")
+	router.HandleFunc("/getBook/{id}", controllers.GetBookById).Methods("Get")
+	router.HandleFunc("/updateBook/{id}", controllers.UpdateBook).Methods("Put")
+	router.HandleFunc("/deleteBook/{id}", controllers.DeleteBook).Methods("Delete")
+	router.HandleFunc("/createBook", controllers.DeleteBook).Methods("Post")
 }
