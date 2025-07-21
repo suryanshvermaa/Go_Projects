901ef694c1215e231148b2da42f4578e01b8d670
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-07-21 15:48:34 +0530

    enhance form handling and update HTML forms for data submission

--- WebServer/main.go
+++ WebServer/main.go
@@ -7,7 +7,32 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if 
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusNotFound)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform error %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post Request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name-%s", name)
+	fmt.Fprintf(w, "Adress-%s", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello")
 }
 
 func main() {
@@ -16,7 +41,7 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Println("server started at port 8000\n")
+	fmt.Println("server started at port 8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
