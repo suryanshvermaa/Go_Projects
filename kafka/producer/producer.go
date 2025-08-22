66e7fb78c71f19c67a88572acea39cbdb1d361bd
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-22 23:19:36 +0530

    initialized project and setup producer

---
+++ kafka/producer/producer.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func ConnectProducer(brokersUri []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersUri, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUri := []string{"localhost:29092"}
+	producer, err := ConnectProducer(brokersUri)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Message is stored in topic {%s}/partition(%d)/offset(%d)", topic, partition, offset)
+	return nil
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"sucess":  false,
+			"message": err,
+		})
+		return err
+	}
+	cmtInBytes, err := json.Marshal(cmt)
+	PushCommentToQueue("comments", cmtInBytes)
+
+	err = c.JSON(&fiber.Map{
+		"sucess":  true,
+		"message": "comments saved sucessfully",
+		"comment": cmt,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"sucess":  true,
+			"message": err,
+		})
+		return err
+	}
+	return nil
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
