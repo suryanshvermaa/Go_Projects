ac2d9d2f4dc1707f3781119520992fda378f4199
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-08-24 10:52:17 +0530

    added dockerfile

--- chi/main.go
+++ chi/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not defined")
+	}
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	fmt.Printf("server is running on port: %s\n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
