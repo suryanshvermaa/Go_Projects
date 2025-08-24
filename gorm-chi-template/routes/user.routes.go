f89c34199fef03a1370729145d0fd07bd5885e4e
ansh verma <suryanshverma.dev.official@gmail.com>
2025-08-24 13:33:06 -0700

    added auth middleware

--- gorm-chi-template/routes/user.routes.go
+++ gorm-chi-template/routes/user.routes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/anshvermaa/go-chi-gorm/controllers"
+	"github.com/anshvermaa/go-chi-gorm/middlewares"
 	"github.com/go-chi/chi"
 )
 
 func UserRoutes() *chi.Mux {
 	userRouter := chi.NewRouter()
+	userRouter.Use(middlewares.AuthMiddleware)
 	userRouter.Get("/getUser", controllers.GetUsers)
 	return userRouter
 }
