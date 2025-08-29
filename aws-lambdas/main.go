3ba1a91793d48687758749e5b399c4cca5f14789
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-08-29 12:18:18 +0530

    added lambda function and trust-policy.json

--- aws-lambdas/main.go
+++ aws-lambdas/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"what is you name?"`
+	Age  int    `json:"what is your age?"`
+}
+
+type MyResponse struct {
+	Message string `json:"answer:"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{
+		Message: fmt.Sprintf("%s is %d year old", event.Name, event.Age),
+	}, nil
+}
+func main() {
+	lambda.Start(HandleLambdaEvent)
 }
