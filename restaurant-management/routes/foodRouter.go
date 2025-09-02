1bd5ee422a128d8665c4dc8c41f6a1a948a1faaa
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-09-03 00:37:32 +0530

    add food and user routes with Gin framework

--- restaurant-management/routes/foodRouter.go
+++ restaurant-management/routes/foodRouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods")
+	incomingRoutes.GET("/foods/:food_id")
+	incomingRoutes.POST("/foods")
+	incomingRoutes.PATCH("/foods")
+}
