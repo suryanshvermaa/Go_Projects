13a42e2fce26dc2cd4256041f9ad1ff11cfc97fc
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-02 21:41:30 +0530

    added gingonics

--- restaurant-management/main.go
+++ restaurant-management/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error in loading .env")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	// auth middleware
+
+	// all routes setup
+
+	router.Run(":" + port)
 }
