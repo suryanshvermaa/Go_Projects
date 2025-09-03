1d4097ceff3fbe9ab939507e5d73e6f3e9ac130f
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-09-03 17:12:52 +0530

    add health check route and organize route files

--- restaurant-management/routes/invoiceRouter.go
+++ restaurant-management/routes/invoiceRouter.go
@@ -1,1 +1,10 @@
 package routes
+
+import "github.com/gin-gonic/gin"
+
+func InvoiceRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/invoices")
+	incomingRoutes.GET("/invoices/:invoice_id")
+	incomingRoutes.POST("/invoices")
+	incomingRoutes.PATCH("/invoices/:invoice_id")
+}
