4a26091232dc9b4263bbde72aae9f1bdb86c48ae
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-03 18:39:11 +0530

    implement CRUD operations and routes for food, invoice, menu, note, order, order item, order table, and user entities

--- restaurant-management/routes/noteRouter.go
+++ restaurant-management/routes/noteRouter.go
@@ -1,10 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/suryanshvermaa/restaurant-management/controllers"
+)
 
 func NoteRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("/notes")
-	incomingRoutes.GET("/notes/:note_id")
-	incomingRoutes.POST("/notes")
-	incomingRoutes.PATCH("/notes/:note_id")
+	incomingRoutes.GET("/notes", controllers.GetNotes())
+	incomingRoutes.GET("/notes/:note_id", controllers.GetNote())
+	incomingRoutes.POST("/notes", controllers.CreateNote())
+	incomingRoutes.PATCH("/notes/:note_id", controllers.UpdateNote())
 }
