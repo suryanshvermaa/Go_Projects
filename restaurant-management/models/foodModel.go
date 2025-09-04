253787622a1db89b89d5b4d538db342e4e12a223
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-04 16:54:12 +0000

    food model added

--- restaurant-management/models/foodModel.go
+++ restaurant-management/models/foodModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Food struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Price      float64            `json:"price" validate:"required"`
+	Food_Image *string            `json:"food_image" validate:"required"`
+	Created_At time.Time          `json:"created_at"`
+	Updated_At time.Time          `json:"updated_at"`
+	Food_Id    *string            `json:"food_id"`
+	Menu_Id    *string            `json:"menu_id"`
+}
