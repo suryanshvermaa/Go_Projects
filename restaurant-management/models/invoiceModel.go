78f7ed50a530b700caa9befa553323d516dba1f8
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-04 16:59:26 +0000

    invoice model added

--- restaurant-management/models/invoiceModel.go
+++ restaurant-management/models/invoiceModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Invoice struct {
+	ID               primitive.ObjectID `bson:"_id"`
+	Invoice_Id       *string            `json:"invoice_id"`
+	Order_Id         *string            `json:"order_id"`
+	Payment_Method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
+	Payment_Status   *string            `json:"payment_status" validate:"eq=PENDING|eq=PAID"`
+	Payment_Due_Date time.Time          `json:"payment_due_date"`
+	Created_At       time.Time          `json:"created_at"`
+	Updated_At       time.Time          `json:"updated_at"`
+}
