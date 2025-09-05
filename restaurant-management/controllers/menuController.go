83c95807061f222fefde12f2322412e5447f14b5
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 12:34:09 +0000

    implemented GetMenu function to fetch a specific menu by ID

--- restaurant-management/controllers/menuController.go
+++ restaurant-management/controllers/menuController.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/suryanshvermaa/restaurant-management/database"
 	"github.com/suryanshvermaa/restaurant-management/helpers"
+	"github.com/suryanshvermaa/restaurant-management/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -33,7 +34,16 @@ func GetMenus() gin.HandlerFunc {
 
 func GetMenu() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		menuId := ctx.Param("menu_id")
+		var menu models.Menu
+		err := foodCollection.FindOne(c, bson.M{"menu_id": menuId}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			helpers.NewError(ctx, 400, err.Error())
+			return
+		}
+		helpers.JsonResponse(ctx, 200, "menu fetched successfully", menu)
 	}
 }
 
