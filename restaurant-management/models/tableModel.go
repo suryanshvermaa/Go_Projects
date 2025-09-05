b2223b761f2d9c7ae7815761a1558cd781130c99
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 07:20:21 +0000

    updated table model

--- restaurant-management/models/tableModel.go
+++ restaurant-management/models/tableModel.go
@@ -8,8 +8,8 @@ import (
 
 type Table struct {
 	ID               primitive.ObjectID `bson:"_id"`
-	Number_Of_Guests *int               `json:"number_of_guests"`
-	Table_Number     *int               `json:"table_number"`
+	Number_Of_Guests *int               `json:"number_of_guests" validate:"required"`
+	Table_Number     *int               `json:"table_number" validate:"required"`
 	Created_At       time.Time          `json:"created_at"`
 	Updated_At       time.Time          `json:"updated_at"`
 	Table_Id         string             `json:"table_id"`
