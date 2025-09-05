e720bbe32e3f8310876755878cd0117c0487035b
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 11:08:29 +0000

    added getfood controller

--- restaurant-management/controllers/foodController.go
+++ restaurant-management/controllers/foodController.go
@@ -1,6 +1,18 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/suryanshvermaa/restaurant-management/database"
+	"github.com/suryanshvermaa/restaurant-management/helpers"
+	"github.com/suryanshvermaa/restaurant-management/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -10,7 +22,16 @@ func GetFoods() gin.HandlerFunc {
 
 func GetFood() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := ctx.Param("food_id")
+		var food models.Food
+		defer cancel()
+		err := foodCollection.FindOne(c, bson.M{"food_id": foodId}).Decode(&food)
+		if err != nil {
+			helpers.NewError(ctx, 400, err.Error())
+			return
+		}
+		helpers.JsonResponse(ctx, 200, "food fetched successfully", food)
 	}
 }
 
