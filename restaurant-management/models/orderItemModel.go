a648ffb961e1f1fea5d2e952b1995a35a2858705
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 07:26:31 +0000

    added orderItem Model

--- restaurant-management/models/orderItemModel.go
+++ restaurant-management/models/orderItemModel.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type OrderItem struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Quantity      *string            `json:"quantity" validate:"required,eq=S|eq=M|eq=L"`
+	Unit_Price    *float64           `json:"unit_price" validate:"required"`
+	Created_At    time.Time          `json:"created_at"`
+	Updated_At    time.Time          `json:"updated_at"`
+	Food_Id       *string            `json:"food_id" validate:"required"`
+	Order_Item_Id string             `json:"order_item_id"`
+	Order_Id      *string            `json:"order_id" validate:"required"`
+}
