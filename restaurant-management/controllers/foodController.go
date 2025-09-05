6ae10fc9d7787feffb73cc0b02b56a6fea7ba62a
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 11:47:21 +0000

    added GetFood, getMenus and CreateFood

--- restaurant-management/controllers/foodController.go
+++ restaurant-management/controllers/foodController.go
@@ -5,14 +5,17 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/suryanshvermaa/restaurant-management/database"
 	"github.com/suryanshvermaa/restaurant-management/helpers"
 	"github.com/suryanshvermaa/restaurant-management/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -25,8 +28,8 @@ func GetFood() gin.HandlerFunc {
 		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		foodId := ctx.Param("food_id")
 		var food models.Food
-		defer cancel()
 		err := foodCollection.FindOne(c, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
 		if err != nil {
 			helpers.NewError(ctx, 400, err.Error())
 			return
@@ -37,7 +40,41 @@ func GetFood() gin.HandlerFunc {
 
 func CreateFood() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var food models.Food
+		var menu models.Menu
 
+		if err := ctx.BindJSON(&food); err != nil {
+			helpers.NewError(ctx, 400, err.Error())
+			return
+		}
+		validationErr := validate.Struct(food)
+		if validationErr != nil {
+			helpers.NewError(ctx, 400, validationErr.Error())
+			return
+		}
+
+		err := menuCollection.FindOne(c, bson.M{"menu_id": food.Menu_Id}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			helpers.NewError(ctx, 500, "menu was not found")
+			return
+		}
+		food.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		food.ID = primitive.NewObjectID()
+		food.Food_Id = food.ID.Hex()
+		var num = toFixed(food.Price, 2)
+		food.Price = &num
+
+		result, inertErr := foodCollection.InsertOne(c, food)
+		if inertErr != nil {
+			helpers.NewError(ctx, 500, "food item is not added")
+			return
+		}
+		defer cancel()
+		helpers.JsonResponse(ctx, 200, "food inserted successfully", result)
 	}
 }
 
@@ -51,6 +88,6 @@ func round(num float64) int {
 
 }
 
-func toFixed(num float64, precision int) float64 {
+func toFixed(num *float64, precision int) float64 {
 
 }
--- restaurant-management/controllers/menuController.go
+++ restaurant-management/controllers/menuController.go
@@ -1,10 +1,33 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/suryanshvermaa/restaurant-management/database"
+	"github.com/suryanshvermaa/restaurant-management/helpers"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			helpers.NewError(ctx, 400, err.Error())
+			return
+		}
+		var allMenus []bson.M
+		if err = result.All(c, &allMenus); err != nil {
+			helpers.NewError(ctx, 400, err.Error())
+			return
+		}
+		helpers.JsonResponse(ctx, 200, "menus fetched successfully", allMenus)
 	}
 }
 
