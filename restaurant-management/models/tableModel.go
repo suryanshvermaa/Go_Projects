0b86cd892aa2779ab4173d685db4b6cbcd736646
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-05 07:16:16 +0000

    added table model

--- restaurant-management/models/tableModel.go
+++ restaurant-management/models/tableModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Table struct {
+	ID               primitive.ObjectID `bson:"_id"`
+	Number_Of_Guests *int               `json:"number_of_guests"`
+	Table_Number     *int               `json:"table_number"`
+	Created_At       time.Time          `json:"created_at"`
+	Updated_At       time.Time          `json:"updated_at"`
+	Table_Id         string             `json:"table_id"`
+}
